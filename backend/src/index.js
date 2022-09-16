dc12166ab3419949816bd8f7ae20eddc98f04fc5
Cosmo Andrade Felix <57240412+CosmoAndrade@users.noreply.github.com>
2022-09-16 20:28:10 -0300

    Adicionando cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const port = 3000;
+const cors = require('cors')
 const app = express();
 const routes = require('./routes');
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
